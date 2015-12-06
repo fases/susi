650c5ebcf97a9d7d1b1ce789b2ba9c090d3ff9bd
Ítalo Vieira <italovieir4@gmail.com>
2015-12-05 22:49:07 -0300

    Faz com que index mostre apenas itens do campus
    É exibido somente itens do campus do usuário logado.

--- src/app/Controller/CampusItemsController.php
+++ src/app/Controller/CampusItemsController.php
@@ -20,10 +20,23 @@ class CampusItemsController extends AppController {
  *
  * @return void
  */
-	public function index() {
-		$this->CampusItem->recursive = 0;
-		$this->set('campusItems', $this->Paginator->paginate());
-	}
+    public function index() {
+
+        $this->recursive = 0;
+        $this->CampusItem->bindModel(
+            array(
+                'belongsTo' => array(
+                    'Campus' => array(
+                        'className'  => 'Campus',
+                        'foreignKey' => false,
+                        'conditions' => 'Campus.id = User.campus_id',
+                    )
+                )
+            )
+        );
+
+    $this->set('campusItems', $this->CampusItem->find('all', array('conditions' => array('Campus.id' => $this->Auth->user('campus_id')))));
+}
 
 /**
  * view method
@@ -32,33 +45,33 @@ class CampusItemsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function view($id = null) {
-		if (!$this->CampusItem->exists($id)) {
-			throw new NotFoundException(__('Invalid campus item'));
-		}
-		$options = array('conditions' => array('CampusItem.' . $this->CampusItem->primaryKey => $id));
-		$this->set('campusItem', $this->CampusItem->find('first', $options));
-	}
+public function view($id = null) {
+    if (!$this->CampusItem->exists($id)) {
+        throw new NotFoundException(__('Invalid campus item'));
+    }
+    $options = array('conditions' => array('CampusItem.' . $this->CampusItem->primaryKey => $id));
+    $this->set('campusItem', $this->CampusItem->find('first', $options));
+}
 
 /**
  * add method
  *
  * @return void
  */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->CampusItem->create();
-			if ($this->CampusItem->save($this->request->data)) {
-				$this->Flash->success(__('The campus item has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The campus item could not be saved. Please, try again.'));
-			}
-		}
-		$items = $this->CampusItem->Item->find('list');
-		$users = $this->CampusItem->User->find('list');
-		$this->set(compact('items', 'users'));
-	}
+public function add() {
+    if ($this->request->is('post')) {
+        $this->CampusItem->create();
+        if ($this->CampusItem->save($this->request->data)) {
+            $this->Flash->success(__('The campus item has been saved.'));
+            return $this->redirect(array('action' => 'index'));
+        } else {
+            $this->Flash->error(__('The campus item could not be saved. Please, try again.'));
+        }
+    }
+    $items = $this->CampusItem->Item->find('list');
+    $users = $this->CampusItem->User->find('list');
+    $this->set(compact('items', 'users'));
+}
 
 /**
  * edit method
@@ -67,25 +80,25 @@ class CampusItemsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function edit($id = null) {
-		if (!$this->CampusItem->exists($id)) {
-			throw new NotFoundException(__('Invalid campus item'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->CampusItem->save($this->request->data)) {
-				$this->Flash->success(__('The campus item has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The campus item could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('CampusItem.' . $this->CampusItem->primaryKey => $id));
-			$this->request->data = $this->CampusItem->find('first', $options);
-		}
-		$items = $this->CampusItem->Item->find('list');
-		$users = $this->CampusItem->User->find('list');
-		$this->set(compact('items', 'users'));
-	}
+public function edit($id = null) {
+    if (!$this->CampusItem->exists($id)) {
+        throw new NotFoundException(__('Invalid campus item'));
+    }
+    if ($this->request->is(array('post', 'put'))) {
+        if ($this->CampusItem->save($this->request->data)) {
+            $this->Flash->success(__('The campus item has been saved.'));
+            return $this->redirect(array('action' => 'index'));
+        } else {
+            $this->Flash->error(__('The campus item could not be saved. Please, try again.'));
+        }
+    } else {
+        $options = array('conditions' => array('CampusItem.' . $this->CampusItem->primaryKey => $id));
+        $this->request->data = $this->CampusItem->find('first', $options);
+    }
+    $items = $this->CampusItem->Item->find('list');
+    $users = $this->CampusItem->User->find('list');
+    $this->set(compact('items', 'users'));
+}
 
 /**
  * delete method
@@ -94,17 +107,54 @@ class CampusItemsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function delete($id = null) {
-		$this->CampusItem->id = $id;
-		if (!$this->CampusItem->exists()) {
-			throw new NotFoundException(__('Invalid campus item'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->CampusItem->delete()) {
-			$this->Flash->success(__('The campus item has been deleted.'));
-		} else {
-			$this->Flash->error(__('The campus item could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+public function delete($id = null) {
+    $this->CampusItem->id = $id;
+    if (!$this->CampusItem->exists()) {
+        throw new NotFoundException(__('Invalid campus item'));
+    }
+    $this->request->allowMethod('post', 'delete');
+    if ($this->CampusItem->delete()) {
+        $this->Flash->success(__('The campus item has been deleted.'));
+    } else {
+        $this->Flash->error(__('The campus item could not be deleted. Please, try again.'));
+    }
+    return $this->redirect(array('action' => 'index'));
+}
+
+
+private function getOnlyCampusItem() {
+    $this->loadModel('User');
+    $users = $this->User->findAllByCampusId($this->Auth->user('campus_id'));
+
+    $campusItems = array();
+    foreach($users as $user) {
+        foreach ($user['CampusItem'] as $campusItem) {
+            $campusItems[] = $campusItem;
+        }
+    }
+    return $campusItems;
+}
+
+private function outraFuncao() {
+    $this->CampusItem->bindModel(
+        array(
+            'belongsTo' => array(
+                'User' => array(
+                    'className'  => 'User',
+                    'foreignKey' => 'user_id',
+                    'fields'     => 'id'
+                ),
+                'Campus' => array(
+                    'className'  => 'Campus',
+                    'foreignKey' => false,
+                    'conditions' => 'Campus.id = User.campus_id',
+                    'fields'     => 'id'
+                )
+            )
+        )
+    );
+
+    return $this->CampusItem->find('all', array('conditions' => array('Campus.id' => $this->Auth->user('campus_id'))));
+}
+
 }
